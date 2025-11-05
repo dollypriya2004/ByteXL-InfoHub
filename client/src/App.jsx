e3bb8df368aa1dda7d7b88f87344cb8782da3970
Dollypriya <114284951+dollypriya2004@users.noreply.github.com>
2025-11-05 16:29:56 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import WeatherModule from './components/WeatherModule';
-import CurrencyConverter from './components/CurrencyConverter';
+import WeatherModule from './components/WeatherModule.jsx';
+import CurrencyConverter from './components/CurrencyConverter.jsx';
 import QuoteGenerator from './components/QuoteGenerator.jsx';
 import './App.css';
 
